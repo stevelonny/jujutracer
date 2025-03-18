583392fba879f6935a1e043d9e70ef4cec25f254
Stefano Lonardoni <steve.lonny@outlook.com>
2025-03-18 12:06:48 +0100

    Add color products and relative tests

--- src/color.jl
+++ src/color.jl
@@ -1,7 +1,9 @@
 # Redefine +,-,* and ≈ for Color types
 # ColorTypes.RGB{T} should be equivalent to RGB
 Base.:*(c::ColorTypes.RGB{T}, scalar::Real) where {T} = ColorTypes.RGB{T}(c.r * scalar, c.g * scalar, c.b * scalar)
+Base.:*(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB{T}(c1.r * c2.r, c1.g * c2.g, c1.b * c2.b)
 Base.:/(c::ColorTypes.RGB{T}, scalar::Real) where {T} = ColorTypes.RGB{T}(c.r / scalar, c.g / scalar, c.b / scalar)
+Base.:/(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB{T}(c1.r / c2.r, c1.g / c2.g, c1.b / c2.b)
 Base.:+(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB{T}(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b)
 Base.:-(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = ColorTypes.RGB{T}(c1.r - c2.r, c1.g - c2.g, c1.b - c2.b)
 Base.:≈(c1::ColorTypes.RGB{T}, c2::ColorTypes.RGB{T}) where {T} = c1.r ≈ c2.r && c1.g ≈ c2.g && c1.b ≈ c2.b
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,12 @@
 using jujutracer
 using Test
 
-@testset "Colors" begin
+@testset "Colors arithmetics" begin
     # Put here the tests required for color sum and product
     @test RGB(1.2, 3.4, 5.6) + RGB(1.2, 3.4, 5.6) ≈ RGB(2.4, 6.8, 11.2)
     @test RGB(2.4, 6.8, 11.2) - RGB(1.2, 3.4, 5.6) ≈ RGB(1.2, 3.4, 5.6)
     @test RGB(1.2, 3.4, 5.6) * 2 ≈ RGB(2.4, 6.8, 11.2)
+    @test RGB(1.2, 3.4, 5.6) * RGB(2.0, 2.0, 2.0) ≈ RGB(2.4, 6.8, 11.2) 
+    @test RGB(2.4, 6.8, 11.2) / RGB(2.0, 2.0, 2.0) ≈ RGB(1.2, 3.4, 5.6)
     @test RGB(2.4, 6.8, 11.2) / 2 ≈ RGB(1.2, 3.4, 5.6)
 end
