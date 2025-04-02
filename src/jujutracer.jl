dff7ccdbf9c0b08f8b5f22a3e6e7fac5eded1ec4
Stefano Lonardoni <steve.lonny@outlook.com>
2025-04-02 09:59:49 +0200

    Remove exports for tone mapping private functions

--- src/jujutracer.jl
+++ src/jujutracer.jl
@@ -1,11 +1,14 @@
 module jujutracer
 
-# imports
+#using
+using Images, FileIO, ImageIO
+
+# import
 import Base:
     +, -, *, ≈, /
 import ColorTypes: ColorTypes, RGB
 import Colors
-##
+
 #export
 export RGB
 
@@ -16,7 +19,7 @@ include("color.jl")
 export _lumi_mean, _lumi_weighted, _lumi_D, _lumi_Func, _RGBluminosity
 
 include("hdrimg.jl")
-export hdrimg, valid_coordinates, InvalidPfmFileFormat, _read_float, _parse_endianness, _parse_image_size, _read_line, read_pfm_image, average_luminosity, normalize_img, _clamp_img
+export hdrimg, valid_coordinates, InvalidPfmFileFormat, _read_float, _parse_endianness, _parse_image_size, _read_line, read_pfm_image
 
 
 end # module jujutracer
