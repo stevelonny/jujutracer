1dec841ccffe21e937b6e1f0af3e91f3cbb30dfe
Stefano Lonardoni <steve.lonny@outlook.com>
2025-04-09 11:18:35 +0200

    Add Normal struct

--- src/geometry.jl
+++ src/geometry.jl
@@ -1,1 +1,14 @@
-#Così steve è contento 
+#Così steve è contento
+
+"""
+    Normal(x::Float64, y::Float64, z::Float64)
+
+Struct representing a normal vector in 3D space.
+# Fields
+- `x::Float64`,`y::Float64`,`z::Float64`: Coordinates.
+"""
+struct Normal
+    x::Float64
+    y::Float64
+    z::Float64
+end
