888d73d595bbac96ca4635087a971b6b16b6a0e6
Michele Boldini <micheleboldini00@yahoo.it>
2025-04-16 10:15:58 +0200

    Implement geometry.jl (#1)
    * Create PR and geometry.jl
    
    * Add Normal struct
    
    * Add struct Point
    
    * Implement Vec type
    
    * Add sum, difference and scalar product methods
    
    * Implement norm squared_norm and normalize! + tests
    
    * Add to_string for geometric types, add scalar prodcut and approx
    
    * Reorder operations, remove unnecessaries tries and catch
    
    * Add negation for Vec and Normal, try to normalize in place Vec or Normal
    
    * Add \cdot and \times product between Vec(s) and Normal(s)
    
    * Add constructors for Vec and Normal, normalize return a vec
    
    * Add tests for \cdot and \times product
    
    * Refactor Vec implementation and update normalization logic, added some tests
    
    * Begin building transformations
    
    * Fix transformations and add some method
    
    * Add application of transformation over Vec/Point/Normale and write test
    
    * Add docstrings to transformations
    
    ---------
    
    Co-authored-by: Michi2600 <michele.boldini@studenti.unimi.it>
    Co-authored-by: Stefano Lonardoni <steve.lonny@outlook.com>
    Co-authored-by: Giovanni Galafassi <giovanni.galafassi@studenti.unimi.it>

---
+++ src/geometry.jl
@@ -0,0 +1,456 @@
+import Base: *, +, -
+
+#--------------------------------------------------------------------------
+# Point type implementation
+#--------------------------------------------------------------------------
+"""
+    Point(x::Float64, y::Float64, z::Float64)
+
+Struct representing a point in 3D space.
+# Fields
+- `x::Float64`,`y::Float64`,`z::Float64`: Coordinates.
+"""
+struct Point
+    x::Float64
+    y::Float64
+    z::Float64
+end
+
+#--------------------------------------------------------------------------
+# Vec type implementation
+#--------------------------------------------------------------------------
+"""
+    Vec(x::Float64, y::Float64, z::Float64)
+
+A struct representing a vector in 3D space.
+# Fields
+- `x::Float64`,`y::Float64`,`z::Float64`: Coordinates.
+# Methods
+- `Vec(n::Normal)`: Create a Vec from a Normal.
+- `Vec(p::Point)`: Create a Vec from a Point.
+"""
+struct Vec
+    x::Float64
+    y::Float64
+    z::Float64
+    function Vec(x, y, z)
+        new(x, y, z)
+    end
+    function Vec(p::Point)
+        new(p.x, p.y, p.z)
+    end
+end
+
+#--------------------------------------------------------------------------
+# Normal type implementation
+#--------------------------------------------------------------------------
+"""
+    Normal(x::Float64, y::Float64, z::Float64)
+
+Struct representing a unit vector (normal) in 3D space.
+# Fields
+- `x::Float64`,`y::Float64`,`z::Float64`: Coordinates.
+# Methods
+- `Normal(v::Vec)`: Create a Normal from a Vec.
+- `Normal(x::Float64, y::Float64, z::Float64)`: Create a Normal from x, y, z coordinates.
+# Throws
+- `ArgumentError`: If the vector is zero.
+"""
+struct Normal
+    x::Float64
+    y::Float64
+    z::Float64
+    function Normal(x, y, z)
+        if x == 0 && y == 0 && z == 0
+            throw(ArgumentError("Normal vector cannot be zero."))
+        end
+        m = sqrt(x^2 + y^2 + z^2)
+        new(x/m, y/m, z/m)
+    end
+    function Normal(v::Vec)
+        if v.x == 0 && v.y == 0 && v.z == 0
+            throw(ArgumentError("Normal vector cannot be zero."))
+        end
+        m = sqrt(v.x^2 + v.y^2 + v.z^2)
+        new(v.x/m, v.y/m, v.z/m)
+    end
+end
+
+# Outside constructors
+function Vec(n::Normal)
+    new(n.x, n.y, n.z)
+end
+
+#--------------------------------------------------------------------------
+# Common methods
+#--------------------------------------------------------------------------
+"""
+    to_string(v::T) where {T<:Union{Point, Vec, Normal}}
+
+Converts a Point, Vec, or Normal object to a string representation.
+# Arguments
+- `v::T`: The object to be converted. Expected to be a struct of 3 values (x, y, z).
+# Returns
+- A string representation of the object.
+"""
+function to_string(v::T) where {T<:Union{Point, Vec, Normal}}
+    return "$(T)(x=$(v.x), y=$(v.y), z=$(v.z))"
+end
+
+"""
+    squared_norm(v::Union{Vec, Normal})
+
+Calculates the squared norm of a vector.
+# Arguments
+- `v::Union{Vec, Normal}`: The vector to be processed. Expected to be a struct of 3 values (x, y, z).
+# Returns
+- The squared norm of the vector.
+"""
+function squared_norm(v::Union{Vec, Normal})
+    return v.x^2 + v.y^2 + v.z^2
+end
+
+
+"""
+    norm(v::Union{Vec, Normal})
+
+Calculates the norm of a vector.
+# Arguments
+- `v::Union{Vec, Normal}`: The vector to be processed. Expected to be a `Vec` or `Normal`  of 3 values (x, y, z).
+# Returns
+- The norm of the vector.
+"""
+function norm(v::Union{Vec, Normal})
+    return sqrt(squared_norm(v))
+end
+
+"""
+    normalize(v::Union{Vec, Normal})
+
+Return a normalized vector.
+# Arguments
+- `v::Vec`: The vector to be normalized.
+# Returns
+- The normalized vector.
+# Throws
+- `ArgumentError`: If the vector is zero.
+"""
+function normalize(v::Union{Vec, Normal})
+    if v.x == 0 && v.y == 0 && v.z == 0
+        return v
+    end
+    return v/norm(v)
+end
+
+
+#--------------------------------------------------------------------------
+# Operations
+#--------------------------------------------------------------------------
+Base.:+(a::T, b::Vec) where {T<:Union{Vec, Point}} = T(a.x + b.x, a.y + b.y, a.z + b.z)
+Base.:+(a::Normal, b::Normal)= Normal(a.x + b.x, a.y + b.y, a.z + b.z)
+Base.:-(a::T, b::Vec) where {T<:Union{Vec, Point}} = T(a.x - b.x, a.y - b.y, a.z - b.z)
+Base.:-(a::Normal, b::Normal) = Normal(a.x - b.x, a.y - b.y, a.z - b.z)
+Base.:-(v::T) where {T<:Union{Vec, Normal}} = T(-v.x, -v.y, -v.z)
+Base.:-(a::Point,b::Point) = Vec(a.x-b.x, a.y-b.y, a.z-b.z)
+Base.:*(a::Union{Vec, Normal}, b::Union{Vec, Normal}) = a.x * b.x + a.y * b.y + a.z * b.z
+Base.:*(v::T, scalar::Real) where {T<:Union{Vec, Normal}} = T(v.x * scalar, v.y * scalar, v.z * scalar)
+Base.:*(scalar::Real, v::T) where {T<:Union{Vec, Normal}} = T(v.x * scalar, v.y * scalar, v.z * scalar) 
+Base.:/(v::T, scalar::Real) where {T<:Union{Vec, Normal}} = T(v.x / scalar, v.y / scalar, v.z / scalar)
+Base.:≈(v1::T, v2::T) where {T<:Union{Point, Vec, Normal}} = v1.x ≈ v2.x && v1.y ≈ v2.y && v1.z ≈ v2.z
+
+"""
+    a ⋅ b
+    
+Return scalar product (``\\cdot``) between `Vec` or `Normal`.
+"""
+function ⋅(a::Union{Vec, Normal}, b::Union{Vec, Normal})
+    return a.x * b.x + a.y * b.y + a.z * b.z
+end
+
+"""
+    a × b
+    
+Return wedge product (``\\times``) between `Vec` or `Normal` as a `Vec`.
+"""
+function ×(a::Union{Vec, Normal}, b::Union{Vec, Normal})
+    return Vec(a.y * b.z - a.z * b.y, a.z*b.x - a.x * b.z, a.x * b.y - a.y * b.x)
+end
+
+#--------------------------------------------------------------------------
+# Transformations
+#--------------------------------------------------------------------------
+
+"""
+    AbstractTransformation
+
+An abstract type that serves as a base for defining various geometric transformations.
+Made concrete by [`Transformation`](@ref), [`Translation`](@ref), [`Scaling`](@ref), [`Rx`](@ref), [`Ry`](@ref), and [`Rz`](@ref).
+"""
+abstract type AbstractTransformation end
+
+
+"""
+    struct Transformation <: AbstractTransformation
+
+Represents a transformation in 3D space with homogeneous coordinates.
+This structure is a subtype of [`AbstractTransformation`](@ref).
+
+# Fields
+- `M::Matrix{Float64}`: The 4x4 transformation matrix.
+- `inv::Matrix{Float64}`: The 4x4 inverse of the transformation matrix.
+
+# Constructors
+- `Transformation()`: Creates an identity transformation where `M` and `inv` are both 4x4 identity matrices.
+- `Transformation(M::Matrix{Float64}, inv::Matrix{Float64})`: Creates a transformation with the given `M` and `inv` matrices.
+Throws an `ArgumentError` if:
+  - `M` or `inv` are not 4x4 matrices.
+  - The last element of `M` or `inv` is not `1.0`.
+  - `M` and `inv` are not inverses of each other.
+
+# Notes
+The `M` and `inv` matrices must satisfy the following conditions:
+- Both must be 4x4 matrices.
+- The last element of both matrices must be `1.0`.
+- The product of `M` and `inv` must be _approximately_ equal to the 4x4 identity matrix.
+"""
+struct Transformation <: AbstractTransformation
+    M::Matrix{Float64}
+    inv::Matrix{Float64}
+    function Transformation()
+        # creating M and inv cloumn by column
+        M=[[1.0, 0.0, 0.0, 0.0] [0.0, 1.0, 0.0, 0.0] [0.0, 0.0, 1.0, 0.0] [0.0, 0.0, 0.0, 1.0]]
+        inv=[[1.0, 0.0, 0.0, 0.0] [0.0, 1.0, 0.0, 0.0] [0.0, 0.0, 1.0, 0.0] [0.0, 0.0, 0.0, 1.0]]
+        new(M,inv)
+    end
+    function Transformation(M::Matrix{Float64}, inv::Matrix{Float64})
+        if size(M) != (4, 4) || size(inv) != (4, 4)
+            throw(ArgumentError("M and inv must be 4x4 matrices."))
+        end
+        if M[4, 4] != 1.0 || inv[4, 4] != 1.0
+            throw(ArgumentError("M and inv must be 4x4 matrices with the last element equal to 1."))
+        end
+        if !isapprox(M * inv, [[1.0, 0.0, 0.0, 0.0] [0.0, 1.0, 0.0, 0.0] [0.0, 0.0, 1.0, 0.0] [0.0, 0.0, 0.0, 1.0]])
+            throw(ArgumentError("M and inv must be inverses of each other."))
+        end
+        new(M, inv)
+    end
+end
+
+"""
+    struct Translation <: AbstractTransformation
+
+Represents a translation in 3D space with homogeneous coordinates.
+This structure is a subtype of [`AbstractTransformation`](@ref).
+
+# Fields
+- `M::Matrix{Float64}`: The 4x4 transformation matrix.
+- `inv::Matrix{Float64}`: The 4x4 inverse transformation matrix.
+
+# Constructors
+- `Translation(dx::Float64, dy::Float64, dz::Float64)`: 
+  Creates a `Translation` object with translation offsets `dx`, `dy`, and `dz` along the x, y, and z axes, respectively.
+
+- `Translation(v::Vec)`:
+  Creates a `Translation` object using a `Vec` object.
+"""
+struct Translation <: AbstractTransformation
+    M::Matrix{Float64}
+    inv::Matrix{Float64}
+
+    function Translation(dx::Float64, dy::Float64, dz::Float64)
+        M = [1.0 0.0 0.0 dx; 0.0 1.0 0.0 dy; 0.0 0.0 1.0 dz; 0.0 0.0 0.0 1.0]
+        Inv = [1.0 0.0 0.0 -dx; 0.0 1.0 0.0 -dy; 0.0 0.0 1.0 -dz; 0.0 0.0 0.0 1.0]
+        new(M, Inv)
+    end
+    function Translation(v::Vec)
+        M = [1.0 0.0 0.0 v.x; 0.0 1.0 0.0 v.y; 0.0 0.0 1.0 v.z; 0.0 0.0 0.0 1.0]
+        Inv = [1.0 0.0 0.0 -v.x; 0.0 1.0 0.0 -v.y; 0.0 0.0 1.0 -v.z; 0.0 0.0 0.0 1.0]
+        new(M, Inv)
+    end
+end
+
+"""
+    struct Scaling <: AbstractTransformation
+
+Represents a scaling transformation in 3D space.
+This structure is a subtype of [`AbstractTransformation`](@ref).
+
+# Fields
+- `M::Matrix{Float64}`: The 4x4 transformation matrix.
+- `inv::Matrix{Float64}`: The 4x4 inverse of the transformation matrix.
+
+# Constructor
+- `Scaling(x::Float64, y::Float64, z::Float64):
+    Creates a `Scaling` instance with scaling factors `x`, `y`, and `z` along the x, y, and z axes, respectively.
+    Throws an `ArgumentError` if any of the scaling factors are zero.
+"""
+struct Scaling <: AbstractTransformation
+    M::Matrix{Float64}
+    inv::Matrix{Float64}
+
+    function Scaling(x::Float64,y::Float64,z::Float64)
+        # Check if the scaling factors are zero
+        if x == 0.0 || y == 0.0 || z == 0.0
+            throw(ArgumentError("Scaling factors cannot be zero."))
+        end
+        # creating M and inv cloumn by column
+        M=[[x, 0.0, 0.0, 0.0] [0.0, y, 0.0, 0.0] [0.0, 0.0, z, 0.0] [0.0, 0.0, 0.0, 1.0]]
+        inv=[[1/x, 0.0, 0.0, 0.0] [0.0, 1/y, 0.0, 0.0] [0.0, 0.0, 1/z, 0.0] [0.0, 0.0, 0.0, 1.0]]
+        new(M,inv)
+    end
+end
+
+"""
+    struct Rx <: AbstractTransformation
+
+Represents a rotation transformation around the x-axis in 3D space.
+This structure is a subtype of [`AbstractTransformation`](@ref).
+
+# Fields
+- `M::Matrix{Float64}`: The 4x4 transformation matrix.
+- `inv::Matrix{Float64}`: The 4x4 inverse of the transformation matrix.
+
+# Constructor
+- `Rx(θ)`: Creates an `Rx` instance for a given rotation `θ` (in radians).
+
+# See also
+- [`Ry`](@ref): For rotation around the y-axis.
+- [`Rz`](@ref): For rotation around the z-axis.
+"""
+struct Rx <: AbstractTransformation
+    M::Matrix{Float64}
+    inv::Matrix{Float64}
+
+    function Rx(θ)
+        # creating M and inv cloumn by column
+        app = convert(Float64, θ)
+        M=[[1.0, 0.0, 0.0, 0.0] [0.0, cos(app), sin(app), 0.0] [0.0, -sin(app), cos(app), 0.0] [0.0, 0.0, 0.0, 1.0]]
+        inv=[[1.0, 0.0, 0.0, 0.0] [0.0, cos(app), -sin(app), 0.0] [0.0, sin(app), cos(app), 0.0] [0.0, 0.0, 0.0, 1.0]]
+        new(M,inv)
+    end
+end
+
+"""
+    struct Ry <: AbstractTransformation
+
+Represents a rotation transformation around the y-axis in 3D space.
+This structure is a subtype of [`AbstractTransformation`](@ref).
+
+# Fields
+- `M::Matrix{Float64}`: The 4x4 transformation matrix.
+- `inv::Matrix{Float64}`: The 4x4 inverse of the transformation matrix.
+
+# Constructor
+- `Ry(θ)`: Creates an `Ry` instance for a given rotation `θ` (in radians).
+
+# See also
+- [`Rx`](@ref): For rotation around the x-axis.
+- [`Rz`](@ref): For rotation around the z-axis.
+"""
+struct Ry <: AbstractTransformation
+    M::Matrix{Float64}
+    inv::Matrix{Float64}
+
+    function Ry(θ)
+        # creating M and inv cloumn by column
+        app = convert(Float64, θ)
+        M=[[cos(app), 0.0, -sin(app), 0.0] [0.0, 1.0, 0.0, 0.0] [sin(app), 0.0, cos(app), 0.0] [0.0, 0.0, 0.0, 1.0]]
+        inv=[[cos(app), 0.0, sin(app), 0.0] [0.0, 1.0, 0.0, 0.0] [-sin(app), 0.0, cos(app), 0.0] [0.0, 0.0, 0.0, 1.0]]
+        new(M,inv)
+    end
+end
+
+"""
+    struct Rz <: AbstractTransformation
+
+Represents a rotation transformation around the z-axis in 3D space.
+This structure is a subtype of [`AbstractTransformation`](@ref).
+
+# Fields
+- `M::Matrix{Float64}`: The 4x4 transformation matrix.
+- `inv::Matrix{Float64}`: The 4x4 inverse of the transformation matrix.
+
+# Constructor
+- `Rz(θ)`: Creates an `Rz` instance for a given rotation `θ` (in radians).
+
+# See also
+- [`Rx`](@ref): For rotation around the x-axis.
+- [`Ry`](@ref): For rotation around the y-axis.
+"""
+struct Rz <: AbstractTransformation
+    M::Matrix{Float64}
+    inv::Matrix{Float64}
+
+    function Rz(θ)
+        # creating M and inv cloumn by column
+        app = convert(Float64, θ)
+        M=[[cos(app), sin(app), 0.0, 0.0] [-sin(app), cos(app), 0.0, 0.0] [0.0, 0.0, 1.0, 0.0] [0.0, 0.0, 0.0, 1.0]]
+        inv=[[cos(app), -sin(app), 0.0, 0.0] [sin(app), cos(app), 0.0, 0.0] [0.0, 0.0, 1.0, 0.0] [0.0, 0.0, 0.0, 1.0]]
+        new(M,inv)
+    end
+end
+
+#--------------------------------------------------------------------------
+# Common methods for transformations
+#--------------------------------------------------------------------------
+"""
+    ⊙(a, b)
+
+Composition of two transformations, where `b` is the first acting on the object and `a` the second.
+"""
+function ⊙(a, b)
+    M::Matrix{Float64} = a.M * b.M
+    inv::Matrix{Float64} = b.inv * a.inv
+    return Transformation(M,inv)
+end
+
+"""
+    inverse(a::AbstractTransformation)
+
+Return the inverse transformation
+"""
+function inverse(a::AbstractTransformation)
+    return Transformation(a.inv,a.M)
+end
+
+"""
+    (t::AbstractTransformation)(v::Vec)
+
+Applies the transformation to a `Vec`.
+"""
+function (t::AbstractTransformation)(v::Vec)
+    # homogeneous coordinates
+    v4 = [v.x; v.y; v.z; 0]
+    v4t = t.M * v4
+    return Vec(v4t[1], v4t[2], v4t[3])
+end
+
+"""
+    (t::AbstractTransformation)(p::Point)
+
+Applies the transformation to a `Point`.
+"""
+function (t::AbstractTransformation)(p::Point)
+    v4 = [p.x; p.y; p.z; 1]
+    v4t = t.M * v4
+    return Point(v4t[1], v4t[2], v4t[3])
+end
+
+"""
+    (t::AbstractTransformation)(n::Normal)
+
+Applies the transformation to a `Normal`.
+"""
+function (t::AbstractTransformation)(n::Normal)
+    v4 = [n.x; n.y; n.z; 1]
+    v4t = transpose(t.inv) * v4
+    return Normal(v4t[1], v4t[2], v4t[3])
+end
+
+"""
+    ≈(a::Union{Transformation, Translation, Scaling, Rx, Ry, Rz}, b::Union{Transformation, Translation, Scaling, Rx, Ry, Rz})
+
+Defines an approximate equality operator `≈` for geometric transformations. 
+Two transformations `a` and `b` are considered approximately equal if both their transformation matrices (`M`) and their inverses (`inv`) are approximately equal.
+"""
+Base.:≈(a::Union{Transformation,Translation,Scaling,Rx,Ry,Rz},b::Union{Transformation,Translation,Scaling,Rx,Ry,Rz}) = a.M ≈ b.M && a.inv ≈ b.inv
--- test/runtests.jl
+++ test/runtests.jl
@@ -167,59 +167,147 @@ end
 
 @testset "Tone mapping" begin
     #Tests for single RGB luminosity functions
-    color = RGB(10.0, 30.0, 50.0)
-    @test jujutracer._lumi_mean(color) ≈ 30.0
-    @test jujutracer._RGBluminosity(color, "M") ≈ 30.0
-    @test jujutracer._lumi_weighted(color) ≈ 26.3
-    @test jujutracer._RGBluminosity(color, "W") ≈ 26.3
-    @test jujutracer._lumi_D(color) ≈ 59.16079783099616
-    @test jujutracer._RGBluminosity(color, "D") ≈ 59.16079783099616
-    color = RGB(10.0, 15.0, 30.0)
-    @test jujutracer._lumi_Func(color) ≈ 20.0
-    @test jujutracer._RGBluminosity(color, "LF") ≈ 20.0
+    @testset "Single RGB luminosity functions" begin
+        color = RGB(10.0, 30.0, 50.0)
+        @test jujutracer._lumi_mean(color) ≈ 30.0
+        @test jujutracer._RGBluminosity(color, "M") ≈ 30.0
+        @test jujutracer._lumi_weighted(color) ≈ 26.3
+        @test jujutracer._RGBluminosity(color, "W") ≈ 26.3
+        @test jujutracer._lumi_D(color) ≈ 59.16079783099616
+        @test jujutracer._RGBluminosity(color, "D") ≈ 59.16079783099616
+        color = RGB(10.0, 15.0, 30.0)
+        @test jujutracer._lumi_Func(color) ≈ 20.0
+        @test jujutracer._RGBluminosity(color, "LF") ≈ 20.0
+        @test_throws ArgumentError jujutracer._RGBluminosity(color, "X")
+    end
 
-    @test_throws ArgumentError jujutracer._RGBluminosity(color, "X")
-
-    #test for _average_luminosity
-    img = hdrimg(2, 1)
-    img.img[1, 1] = RGB(5.0, 10.0, 15.0)   #mean luminosity = 10
-    img.img[1, 2] = RGB(50.0, 100.0, 150.0) #mean luminosity = 100
-    @test jujutracer._average_luminosity(img, delta=0.0) ≈ 10^1.5
-
-    @test_throws ArgumentError jujutracer._average_luminosity(img, type="X", delta=0)
-    @test_throws ArgumentError jujutracer._average_luminosity(img, delta="prova")
-    @test_throws ArgumentError jujutracer._average_luminosity(img, delta=-1.0)
-
-    img.img[1, 1] = RGB(0.0, 0.0, 0.0)   #activating delta
-    img.img[1, 2] = RGB(5.0e3, 1.0e4, 1.5e4) #mean luminosity = 1e4
-    @test jujutracer._average_luminosity(img) ≈ 1
+    @testset "Average luminosity" begin
+        img = hdrimg(2, 1)
+        img.img[1, 1] = RGB(5.0, 10.0, 15.0)   #mean luminosity = 10
+        img.img[1, 2] = RGB(50.0, 100.0, 150.0) #mean luminosity = 100
+        @test jujutracer._average_luminosity(img, delta=0.0) ≈ 10^1.5
+        @test_throws ArgumentError jujutracer._average_luminosity(img, type="X", delta=0)
+        @test_throws ArgumentError jujutracer._average_luminosity(img, delta="prova")
+        @test_throws ArgumentError jujutracer._average_luminosity(img, delta=-1.0)
+        img.img[1, 1] = RGB(0.0, 0.0, 0.0)   #activating delta
+        img.img[1, 2] = RGB(5.0e3, 1.0e4, 1.5e4) #mean luminosity = 1e4
+        @test jujutracer._average_luminosity(img) ≈ 1
+    end
 
     #test for _normalize_img
-    img = hdrimg(1, 1)
-    img.img[1, 1] = RGB(10.0, 20.0, 30.0)   #luminosity = 20
-    @test_throws ArgumentError jujutracer._normalize_img!(img, a=-1.0)
-    @test_throws ArgumentError jujutracer._normalize_img!(img, lum="prova")
-
-    jujutracer._normalize_img!(img, a=2 , lum =10 )
-    @test img.img[1,1] ≈ RGB(2.0, 4.0, 6.0) 
+    @testset "Normalize image" begin
+        img = hdrimg(1, 1)
+        img.img[1, 1] = RGB(10.0, 20.0, 30.0)   #luminosity = 20
+        @test_throws ArgumentError jujutracer._normalize_img!(img, a=-1.0)
+        @test_throws ArgumentError jujutracer._normalize_img!(img, lum="prova")
+        jujutracer._normalize_img!(img, a=2 , lum =10 )
+        @test img.img[1,1] ≈ RGB(2.0, 4.0, 6.0) 
+    end
 
     # test for clamp_img
-    img = hdrimg(1, 1)
-    img.img[1, 1] = RGB(10.0, 20.0, 30.0)
-    jujutracer._clamp_img!(img)
-    @test img.img[1, 1] ≈ RGB(10.0/(1+10.0), 20.0/(1+20.0), 30.0/(1+30.0))
+    @testset "Clamp image" begin
+        img = hdrimg(1, 1)
+        img.img[1, 1] = RGB(10.0, 20.0, 30.0)
+        jujutracer._clamp_img!(img)
+        @test img.img[1, 1] ≈ RGB(10.0/(1+10.0), 20.0/(1+20.0), 30.0/(1+30.0))
+    end
 
     # test for gamma correction
-    img = hdrimg(1, 1)
-    img.img[1, 1] = RGB(10.0, 20.0, 30.0)
-    jujutracer._γ_correction!(img; γ = 2.2)
-    @test img.img[1, 1] ≈ RGB(10.0^(1/2.2), 20.0^(1/2.2), 30.0^(1/2.2))
-    img.img[1, 1] = RGB(10.0, 20.0, 30.0)
-    jujutracer._γ_correction!(img; γ = 1.0)
-    @test img.img[1, 1] ≈ RGB(10.0, 20.0, 30.0)
+    @testset "Gamma correction" begin
+        img = hdrimg(1, 1)
+        img.img[1, 1] = RGB(10.0, 20.0, 30.0)
+        jujutracer._γ_correction!(img; γ = 2.2)
+        @test img.img[1, 1] ≈ RGB(10.0^(1/2.2), 20.0^(1/2.2), 30.0^(1/2.2))
+        img.img[1, 1] = RGB(10.0, 20.0, 30.0)
+        jujutracer._γ_correction!(img; γ = 1.0)
+        @test img.img[1, 1] ≈ RGB(10.0, 20.0, 30.0)
+        @test_throws ArgumentError jujutracer._γ_correction!(img; γ = -1.0)
+        @test_throws ArgumentError jujutracer._γ_correction!(img; γ = 0.0)
+        @test_throws ArgumentError jujutracer._γ_correction!(img; γ = "prova")
+    end
+end
 
-    @test_throws ArgumentError jujutracer._γ_correction!(img; γ = -1.0)
-    @test_throws ArgumentError jujutracer._γ_correction!(img; γ = 0.0)
-    @test_throws ArgumentError jujutracer._γ_correction!(img; γ = "prova")
+@testset "Geometry" begin
+    v1 = Vec(1.0, 2.0, 3.0)
+    v2 = Vec(1.0, 2.0, 3.0)
+    v3 = Vec(2.0, 4.0, 6.0)
+    @test v1 ≈ v2
+    @test !(v1 ≈ v3)
+    @test v1 * 2 ≈ Vec(2.0, 4.0, 6.0)
+    @test v1 / 2 ≈ Vec(0.5, 1.0, 1.5)
+    @test v1 + v2 ≈ Vec(2.0, 4.0, 6.0)
+    @test v1 - v2 ≈ Vec(0, 0, 0)
+    @test v1 ⋅ v2 ≈ 14
+    @test -v1 ≈ Vec(-1.0, -2.0, -3.0)
+    a = Vec(1.0, 0.0, 0.0)
+    b = Vec(0.0, 1.0, 0.0)
+    c = Vec(0.0, 0.0, 1.0)
+    @test a × b ≈ c
+    @test b × c ≈ a
+    @test c × a ≈ b
+    @test c × b ≈ -a
+    point = Point(1.0, 2.0, 3.0)
+    @test_throws MethodError v1 ≈ point
+    @test_throws MethodError v1 * 2 ≈ Point(2.0, 4.0, 6.0)
+    @test_throws MethodError v1 / 2 ≈ Point(0.5, 1.0, 1.5)
+    @test_throws ArgumentError Normal(0.0, 0.0, 0.0)
+    normal = Normal(1.0, 2.0, 3.0)
+    @test -normal ≈ Normal(-1.0, -2.0, -3.0)
+    @test_throws MethodError v1 ≈ normal
+    @test normal * 2 ≈ Normal(2.0, 4.0, 6.0)
+    @test normal / 2 ≈ Normal(0.5, 1.0, 1.5)
+    n2 = Normal(1.0, 2.0, 3.0)
+    @test normal ≈ n2
+    p1 = Point(1.0, 2.0, 3.0)
+    p2 = Point(1.0, 2.0, 3.0)
+    @test p1 ≈ p2
+    @test_throws MethodError p1 * 2 
+    @test_throws MethodError p1 / 2
 
+    #Test for Norm 
+    v = Vec(1.0, 2.0, 3.0)
+    n = Normal(10.0, 20.0, 30.0)
+    p = Point(1.0, 2.0, 3.0)
+    @test squared_norm(v) ≈ 14
+    @test norm(v) ≈ sqrt(14)
+    @test squared_norm(n) ≈ 1
+    @test norm(n) ≈ 1
+
+    @test_throws MethodError squared_norm(p)
+    @test_throws MethodError norm(p)
+    
+    #Test for normalize
+    v = Vec(3.0, 4.0, 0.0)
+    n = Normal(3.0, 4.0, 0.0)
+    v0 = normalize(v)
+    @test v0 ≈ Vec(0.6, 0.8, 0.0)
+    @test n.x ≈ 0.6 && n.y ≈ 0.8 && n.z ≈ 0.0
+    n = Normal(v)
+    @test n.x ≈ 0.6 && n.y ≈ 0.8 && n.z ≈ 0.0
+    v0 = Vec(0.0, 0.0, 0.0)
+    @test normalize(v0) ≈ v0
+end
+
+@testset "Transformations" begin
+    a = Vec(1.,0.,0.)
+    b = Point(1.,2.,3.)
+    c = Normal(0.,1.,0.)
+    Id = Transformation()
+    t1 = Translation(a)
+    t2 = Translation(-a)
+    @test t1 ⊙ t2 ≈ Id
+    @test inverse(t1) ≈ t2
+    @test t1(a) ≈ a
+    @test t1(b) ≈ Point(2.,2.,3.)
+    @test t2(c) ≈ c
+
+    s1 = Scaling(2.,1.,1.)
+    s2 = inverse(s1)
+    @test s2 ≈ Scaling(0.5,1.,1.)
+    @test s2(a) ≈ Vec(0.5,0.,0.)
+    @test s2(b) ≈ Point(0.5,2.,3.)
+    @test s2(c) ≈ c
+
+    @test Rx(π) ≈ Scaling(1.,-1.,-1.)
+    @test Rx(π/2) ⊙ Rx(π/2) ⊙ inverse(Rx(π)) ≈ Id
 end
