d71bba52d29bddb4d5f4e2c077aebc7e2d505654
Giovanni Galafassi <giovanni.galafassi@studenti.unimi.it>
2025-04-30 11:17:07 +0200

    Camera (#2)
    * Create camera
    
    * Add macOS-13 (intel) and removed useless julia-arch matrix
    
    * Implement Ray in camera.jl + tests
    
    * Implement cameras
    
    * Add ImageTracer w/ tests and docstrings
    
    * Small typo
    
    * Add last doc-strings
    
    ---------
    
    Co-authored-by: Stefano Lonardoni <steve.lonny@outlook.com>
    Co-authored-by: MichiB2600 <micheleboldini00@yahoo.it>

---
+++ src/camera.jl
@@ -0,0 +1,112 @@
+#----------------------------------------------------
+#Ray 
+#----------------------------------------------------
+"""
+    Ray(origin::Point, dir::Vec, tmin::Float=1e-5, tmax::Float=Inf, depth::Int=0)
+
+A struct representing a ray in 3D space.
+# Fields
+- `origin::Point`: The origin point of the ray.
+- `dir::Vec`: The direction vector of the ray.
+- `tmin::Float`: The minimum distance along the ray (default is `le-5`).
+- `tmax::Float`: The maximum distance along the ray (default is `Inf`).
+- `depth::Int`: The depth of the ray (default is `0`).
+"""
+struct Ray
+    origin::Point
+    dir::Vec 
+    tmin::Float64 
+    tmax::Float64
+    depth::Int
+    #Should vec be normalised?
+
+    function Ray(;origin::Point, dir::Vec, tmin=1e-5, tmax=Inf, depth=0)
+        new(origin, dir, tmin, tmax, depth)
+    end
+
+end
+
+Base.:≈(r1::Ray, r2::Ray) = r1.origin ≈ r2.origin && r1.dir ≈ r2.dir
+
+"""
+    (r::Ray)(t::Float64)
+
+Return the point along the ray at distance `t` from the origin.
+"""
+function (r::Ray)(t::Float64)
+    if t < r.tmin || t > r.tmax
+        throw(ArgumentError("t must be in the range [$(r.tmin), $(r.tmax)]"))
+    else
+        return r.origin + t * r.dir
+    end
+end
+
+#--------------------------------------------------------------------------
+# Camera type
+#--------------------------------------------------------------------------
+"""
+    abstract type AbstractCamera
+Abstact type AbstractCamera
+
+`AbstractCamera(u, v)` returns a ray fired in the pixel (u, v) of the screen
+"""
+abstract type AbstractCamera end
+
+"""
+    Orthogonal
+Orthogonal camera type
+
+# Fields 
+- `t::Transformation` the transformation related to the camera's standing
+- `a_ratio::Float64` aspect ratio of the screen of the camera
+# Constructor 
+- `Orthogonal()` creates an orthogonal camera with Identity `Transformation` and a 16:9 aspect ratio
+- `Ortoghonal(t = trans, a_ratio = a)` creates an orthogonal camera with trans `Transformation` and an `a` aspect ratio
+"""
+struct Orthogonal <: AbstractCamera
+    t::Transformation
+    a_ratio::Float64 
+    function Orthogonal(;t::Transformation = Transformation(), a_ratio::Float64 = 16//9)
+        new(t, a_ratio)
+    end
+end
+
+"""
+    Perspective
+Perspective camera type
+
+# Fields 
+- `t::Transformation` the transformation related to the camera's standing
+- `a_ratio::Float64` aspect ratio of the screen of the camera
+# Constructor 
+- `Perspective()` creates a perspective camera with Identity `Transformation`, a 16:9 aspect ratio placed in (-1, 0, 0)
+- `Perspective(d = dist, t = trans, a_ratio = a)` creates a perspective camera with trans `Transformation`, an `a` aspect ratio placed in (-d, 0, 0)
+"""
+struct Perspective <: AbstractCamera
+    d::Float64
+    t::Transformation 
+    a_ratio::Float64 
+    function Perspective(;d::Float64 = 1., t::Transformation = Transformation(), a_ratio::Float64 = 16//9)
+        new(d, t, a_ratio)
+    end
+end
+
+function (c::AbstractCamera)(u::Float64, v::Float64)
+    return Ray(origin = c.t(_origin(c,u,v)),dir = c.t(_direction(c,u,v)), tmin = 1.0e-5)
+end
+
+function _origin(p::Perspective,u::Float64,v::Float64)
+    return Point(-p.d,0.0,0.0)
+end
+
+function _direction(p::Perspective,u::Float64,v::Float64)
+    return Vec(p.d, (1.0 - 2 * u) * p.a_ratio, 2 * v - 1)
+end
+
+function _origin(o::Orthogonal,u::Float64,v::Float64)
+    return Point(-1.0, (1.0 - 2 * u) * o.a_ratio, 2 * v - 1)
+end
+
+function _direction(o::Orthogonal,u::Float64,v::Float64)
+    return Vec(1.,0.,0.)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -311,3 +311,76 @@ end
     @test Rx(π) ≈ Scaling(1.,-1.,-1.)
     @test Rx(π/2) ⊙ Rx(π/2) ⊙ inverse(Rx(π)) ≈ Id
 end
+
+@testset "Ray" begin
+    a = Vec(1.0,0.0,0.0)
+    b = Point(1.,2.,3.)
+    r = Ray(origin=b, dir=a)
+    r1= Ray(origin=b, dir=a)
+    @test r1 ≈ r
+    @test r(1.) ≈ Point(2.,2.,3.)
+    @test_throws ArgumentError r(0.)    
+    @test !(r(1.) ≈ r1.origin)
+end
+
+@testset "Cameras" begin
+    t = Transformation()
+    cam = Orthogonal(a_ratio = 2.)
+    ray1 = cam(0.,0.)
+    ray2 = cam(1.,0.)
+    ray3 = cam(0.,1.)
+    ray4 = cam(1.,1.)
+
+    @test squared_norm(ray1.dir × ray2.dir) ≈ 0.
+    @test squared_norm(ray2.dir × ray3.dir) ≈ 0.
+    @test squared_norm(ray3.dir × ray4.dir) ≈ 0.
+
+    @test ray1(1.) ≈ Point(0.,2.,-1.)
+    @test ray2(1.) ≈ Point(0.,-2.,-1.)
+    @test ray3(1.) ≈ Point(0.,2.,1.)
+    @test ray4(1.) ≈ Point(0.,-2.,1.)
+
+    cam = Perspective(d = 1., a_ratio = 2.)
+    ray1 = cam(0.,0.)
+    ray2 = cam(1.,0.)
+    ray3 = cam(0.,1.)
+    ray4 = cam(1.,1.)
+
+    @test ray1.origin ≈ ray2.origin
+    @test ray2.origin ≈ ray3.origin
+    @test ray3.origin ≈ ray4.origin
+
+    @test ray1(1.) ≈ Point(0.,2.,-1.)
+    @test ray2(1.) ≈ Point(0.,-2.,-1.)
+    @test ray3(1.) ≈ Point(0.,2.,1.)
+    @test ray4(1.) ≈ Point(0.,-2.,1.)
+end
+
+@testset "ImageTracer" begin
+    img = hdrimg(4, 2)
+    cam = Perspective(a_ratio = 2.0)
+    tracer = ImageTracer(img, cam)
+
+    ray1 = tracer(0, 0; u_pixel=2.5, v_pixel=1.5)
+    ray2 = tracer(2, 1; u_pixel=0.5, v_pixel=0.5)
+    @test ray1 ≈ ray2
+
+    tracer(ray -> RGB(1.0, 2.0, 3.0))
+    for row in 1:img.h
+        for col in 1:img.w
+            @test img.img[row, col] ≈ RGB(1.0, 2.0, 3.0)
+        end
+    end
+
+    cam = Orthogonal(a_ratio = 2.0)
+    tracer = ImageTracer(img, cam)
+    tracer(ray -> RGB(1.0, 2.0, 3.0))
+    for row in 1:img.h
+        for col in 1:img.w
+            @test img.img[row, col] ≈ RGB(1.0, 2.0, 3.0)
+        end
+    end
+
+    inv_fun = (x::Int64 -> RGB(1.0, 2.0, 3.0))
+    @test_throws MethodError tracer(inv_fun)
+end
