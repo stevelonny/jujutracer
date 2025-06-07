b036d4bdb9213a10340a36f7a7dcc1c23231418d
Stefano Lonardoni <steve.lonny@outlook.com>
2025-06-07 15:05:04 +0200

    CI: up size of HTML pages

--- docs/make.jl
+++ docs/make.jl
@@ -3,12 +3,13 @@ using Documenter
 using jujutracer  # Replace with your actual package name
 
 makedocs(
-    sitename = "jujutracer",
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true"
+    sitename="jujutracer",
+    format=Documenter.HTML(
+        prettyurls=get(ENV, "CI", nothing) == "true",
+        size_threshold=500_000
     ),
-    modules = [jujutracer],
-    pages = [
+    modules=[jujutracer],
+    pages=[
         "Home" => "index.md",
         "Detailed API" => "detail.md"
         # Add your other pages here
@@ -16,6 +17,6 @@ makedocs(
 )
 
 deploydocs(
-    repo = "github.com/stevelonny/jujutracer.git",
-    versions = ["stable" => "v^", "v#.#", "dev" => "main"]
+    repo="github.com/stevelonny/jujutracer.git",
+    versions=["stable" => "v^", "v#.#", "dev" => "main"]
 )
