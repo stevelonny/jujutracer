48e2c1a174f69ae7ce81473d8df436007adb224c
Giovanni Galafassi <giovanni.galafassi@studenti.unimi.it>
2025-06-08 12:40:33 +0200

    Change the architecture of mesh but still some problem is present

--- src/shapes/meshes.jl
+++ src/shapes/meshes.jl
@@ -6,29 +6,28 @@ struct mesh <: AbstractShape
     file::String
     shapes::Vector{Triangle}
     points::Vector{Point}
-    box::AABB
 
     function mesh(file::String, shapes::Vector{Triangle}, points::Vector{Point})
-        new(file, shapes, points, AABB(shapes))
+        new(file, shapes, points)
     end
     function mesh(file::String)
         sh, p = read_obj_file(file)
-        new(file, sh, p, AABB(sh))
+        new(file, sh, p)
     end
 
     function mesh(file::String, Tr::AbstractTransformation)
         sh, p = read_obj_file(file, Tr = Tr)
-        new(file, sh, p, AABB(sh))
+        new(file, sh, p)
     end
 
     function mesh(file::String, Mat::Material)
         sh, p = read_obj_file(file, Mat = Mat)
-        new(file, sh, p, AABB(sh))
+        new(file, sh, p)
     end
 
     function mesh(file::String, Tr::AbstractTransformation, Mat::Material)
         sh, p = read_obj_file(file, Tr = Tr, Mat = Mat)
-        new(file, sh, p, AABB(sh))
+        new(file, sh, p)
     end
 end
 
@@ -40,8 +39,8 @@ Works only convex poygons
 function trianglize(P::Vector{Point}, Tr::AbstractTransformation, Mat::Material)
     tr = Vector{Triangle}()
     p = P[1]
-    sort!(P, by=x -> squared_norm(x - p))
-    for i in 2:(length(P) - 2)
+    sort!(P, by=x -> norm(x - p))
+    for i in 1:(length(P) - 2)
         add = Triangle(Tr, p, P[i+1], P[i+2], Mat)
         push!(tr, add)
     end
@@ -64,23 +63,23 @@ function read_obj_file(io::IOBuffer; Tr::AbstractTransformation = Transformation
             push!(points, p)
         elseif line[1] == "f"
             # add a shape
-            tr = nothing
             if dim == 4
                 tr = Triangle(Tr,
                               points[parse(Int, line[2])],
                               points[parse(Int, line[3])],
                               points[parse(Int, line[4])],
                               Mat)
+                push!(shapes, tr)
             elseif dim > 4
                 P = Vector{Point}()
                 for i in 2:dim
                     push!(P, points[parse(Int, line[i])])
                 end
                 tr = trianglize(P, Tr, Mat)
+                shapes = vcat(shapes, tr)
             else
                 throw(ArgumentError("Invalid polygon declaration"))
             end
-            push!(shapes, tr)
         end
     end
 
@@ -102,7 +101,18 @@ function read_obj_file(filename::String; Tr::AbstractTransformation = Transforma
 end
 
 function ray_intersection(S::mesh, ray::Ray)
-    return ray_intersection(S.box, ray)
+    dim = length(S.shapes)
+        closest = nothing
+        for i in 1:dim
+            inter = ray_intersection(S.shapes[i], ray)
+            if isnothing(inter)
+                continue
+            end
+            if (isnothing(closest) || inter.t < closest.t)
+                closest = inter
+            end
+        end
+    return closest
 end
 
 """
@@ -115,5 +125,12 @@ Returns the two points defining the axis-aligned bounding box (AABB) that contai
 - `Tuple{Point, Point}`: a tuple containing the two points defining the AABB, where the first point is the minimum corner and the second point is the maximum corner.
 """
 function boxed(m::mesh)::Tuple{Point,Point}
-    return (m.box.P1, m.box.P2)
+    P1 = Point(Inf, Inf, Inf)
+    P2 = Point(-Inf, -Inf, -Inf)
+    for s in m.shapes
+        p1, p2 = boxed(s)
+        P1 = Point(min(P1.x, p1.x), min(P1.y, p1.y), min(P1.z, p1.z))
+        P2 = Point(max(P2.x, p2.x), max(P2.y, p2.y), max(P2.z, p2.z))
+    end
+    return P1, P2
 end
