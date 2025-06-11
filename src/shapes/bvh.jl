26745ca1e0e1df7cee83433b1c16a52a183b8b7a
Stefano Lonardoni <steve.lonny@outlook.com>
2025-06-11 11:03:37 +0200

    Fix (I think?) SubdivideSAH!

--- src/shapes/bvh.jl
+++ src/shapes/bvh.jl
@@ -10,6 +10,10 @@ mutable struct BVHNode
     end
 end
 
+function Base.show(io::IO, node::BVHNode)
+    print(io, "BVHNode(p_min=$(node.p_min), p_max=$(node.p_max), first_index=$(node.first_index), last_index=$(node.last_index))")
+end
+
 function centroid(t::AbstractShape)
     p1, p2 = boxed(t)
     return Point((p1.x + p2.x) / 2.0, (p1.y + p2.y) / 2.0, (p1.z + p2.z) / 2.0)
@@ -18,6 +22,20 @@ end
 function surface_area(p1::Point, p2::Point)
     diagonal = p2 - p1
     return 2 * (diagonal.x * diagonal.y + diagonal.x * diagonal.z + diagonal.y * diagonal.z)
+end
+
+function offset(p::Point, pmin::Point, pmax::Point)
+    o = p - pmin
+    if pmax.x > pmin.x
+        o = Point(o.x / (pmax.x - pmin.x), o.y, o.z)
+    end
+    if pmax.y > pmin.y
+        o = Point(o.x, o.y / (pmax.y - pmin.y), o.z)
+    end
+    if pmax.z > pmin.z
+        o = Point(o.x, o.y, o.z / (pmax.z - pmin.z))
+    end
+    return o
 end
 
 function UpdateBoundaries!(node::BVHNode, shapes::Vector{AbstractShape})
@@ -32,12 +50,13 @@ function UpdateBoundaries!(node::BVHNode, shapes::Vector{AbstractShape})
     node.p_max = pmax
 end
 
-function Subdivide!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::Vector{Point}; max_shapes_per_leaf::Int64=2)
+function Subdivide!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::Vector{Point}, depth::Int64; max_shapes_per_leaf::Int64=2)
+    depth += 1
     if node.last_index - node.first_index + 1 <= max_shapes_per_leaf
         # create a leaf node
         node.left = nothing
         node.right = nothing
-        return
+        return depth
     end
 
     extent = node.p_max - node.p_min
@@ -66,7 +85,7 @@ function Subdivide!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::Vec
     end
 
     if left_index - node.first_index <= 0 || node.last_index - right_index <= 0
-        return
+        return depth
     end
 
     node.left = BVHNode(node.first_index, left_index - 1)
@@ -74,14 +93,22 @@ function Subdivide!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::Vec
     UpdateBoundaries!(node.left, shapes)
     UpdateBoundaries!(node.right, shapes)
 
-    Subdivide!(node.left, shapes, centroids; max_shapes_per_leaf=max_shapes_per_leaf)
-    Subdivide!(node.right, shapes, centroids; max_shapes_per_leaf=max_shapes_per_leaf)
+    left_depth = Subdivide!(node.left, shapes, centroids, depth; max_shapes_per_leaf=max_shapes_per_leaf)
+    right_depht = Subdivide!(node.right, shapes, centroids, depth; max_shapes_per_leaf=max_shapes_per_leaf)
+
+    # return the maximum depth of the tree
+    if left_depth > right_depht
+        return left_depth
+    else
+        return right_depht
+    end
 end
 
-function SubdivideSAH!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::Vector{Point}; max_shapes_per_leaf::Int64=2)
+function SubdivideSAH!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::Vector{Point}, depth::Int64; max_shapes_per_leaf::Int64=2, n_buckets::Int64=12)
+    depth += 1
     # stop if there are too few shapes
     if node.last_index - node.first_index + 1 <= max_shapes_per_leaf
-        return
+        return depth
     end
 
     # chose split axis
@@ -108,19 +135,18 @@ function SubdivideSAH!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::
             node.right = BVHNode(node.first_index + 1, node.last_index)
             UpdateBoundaries!(node.left, shapes)
             UpdateBoundaries!(node.right, shapes)
-            return
+            return depth
         else # num_shapes == 1
             node.left = BVHNode(node.first_index, node.first_index)
             node.right = nothing
             UpdateBoundaries!(node.left, shapes)
-            return
+            return depth + 1
         end
     else
         # initialize buckets for SAH
         p_a_min = getfield(node.p_min, axis)
         p_a_max = getfield(node.p_max, axis)
         extent_axis = p_a_max - p_a_min
-        n_buckets = 12
         bucket_count = Vector{Int64}(undef, n_buckets)
         bucket_bounds = Vector{Tuple{Point,Point}}(undef, n_buckets)
         for i in 1:n_buckets
@@ -128,7 +154,7 @@ function SubdivideSAH!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::
         end
         for i in node.first_index:node.last_index
             centroid_a = getfield(centroids[i], axis)
-            b = floor(Int, (centroid_a - p_a_min) / extent_axis * n_buckets) + 1
+            b = floor(Int64, n_buckets * getfield(offset(centroids[i], node.p_min, node.p_max), axis)) + 1
             if b < 1
                 b = 1
             elseif b > n_buckets
@@ -146,7 +172,13 @@ function SubdivideSAH!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::
                 Point(max(bucket_bounds[b][2].x, P2.x), max(bucket_bounds[b][2].y, P2.y), max(bucket_bounds[b][2].z, P2.z))
             )
         end
-
+        #@debug "Bucket counts and bounds: $(bucket_count) $(bucket_bounds)"
+        for i in 1:n_buckets
+            if bucket_count[i] == 0
+                # no shapes in this bucket, set bounds to empty
+                bucket_bounds[i] = (Point(Inf, Inf, Inf), Point(-Inf, -Inf, -Inf))
+            end
+        end
         # forward scan to assing costs (first part of the cost equation)
         n_splits = n_buckets - 1
         costs = Vector{Float64}(undef, n_splits)
@@ -171,7 +203,6 @@ function SubdivideSAH!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::
             count_above += bucket_count[i+1]
             costs[i] += count_above * surface_area(bound_above[1], bound_above[2])
         end
-
         # find the best split
         best_bucket = 0 #-1
         best_cost = Inf
@@ -190,12 +221,19 @@ function SubdivideSAH!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::
         #   - the cost is lower than the leaf cost
         #   - or the number of shapes is greater than max_shapes_per_leaf
         # - or create a leaf node
-        if (best_cost < leaf_cost || num_shapes > max_shapes_per_leaf)
+        #@debug "Processing node with:" n_buckets=n_buckets p_max = node.p_max p_min = node.p_min axis = axis best_bucket = best_bucket best_cost = best_cost leaf_cost = leaf_cost left= node.first_index right = node.last_index
+        if best_cost < leaf_cost
             # reorder shapes and centroids
             left_index = node.first_index
             right_index = node.last_index
             while left_index <= right_index
-                if getfield(centroids[left_index], axis) < p_a_min + extent_axis * best_bucket / n_buckets
+                b = floor(Int64, n_buckets * getfield(offset(centroids[left_index], node.p_min, node.p_max), axis)) + 1
+                if b < 1
+                    b = 1
+                elseif b > n_buckets
+                    b = n_buckets
+                end
+                if b <= best_bucket
                     left_index += 1
                 else
                     if left_index < right_index
@@ -206,7 +244,8 @@ function SubdivideSAH!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::
                 end
             end #while
             if left_index - node.first_index <= 0 || node.last_index - right_index <= 0
-                return
+                #@debug "No valid split found, creating leaf node"
+                return depth
             end
 
             node.left = BVHNode(node.first_index, left_index - 1)
@@ -215,31 +254,37 @@ function SubdivideSAH!(node::BVHNode, shapes::Vector{AbstractShape}, centroids::
             UpdateBoundaries!(node.left, shapes)
             UpdateBoundaries!(node.right, shapes)
 
-            SubdivideSAH!(node.left, shapes, centroids; max_shapes_per_leaf=max_shapes_per_leaf)
-            SubdivideSAH!(node.right, shapes, centroids; max_shapes_per_leaf=max_shapes_per_leaf)
+            left_depth = SubdivideSAH!(node.left, shapes, centroids, depth; max_shapes_per_leaf=max_shapes_per_leaf)
+            right_depth = SubdivideSAH!(node.right, shapes, centroids, depth; max_shapes_per_leaf=max_shapes_per_leaf)
+
+            # return the maximum depth of the tree
+            if left_depth > right_depth
+                return left_depth
+            else
+                return right_depth
+            end
+
         else # create a leaf node
-            node.left = nothing
-            node.right = nothing
-            return
+            return depth
         end
     end
 end
 
-function BuildBVH(shapes::Vector{AbstractShape}; use_sah::Bool=false, max_shapes_per_leaf::Int64=2)
+function BuildBVH(shapes::Vector{AbstractShape}; use_sah::Bool=false, max_shapes_per_leaf::Int64=2, n_buckets::Int64=12)
     root = BVHNode(1, length(shapes))
-    @debug "Building BVH tree" shapes = length(shapes) max_shapes_per_leaf = max_shapes_per_leaf use_sah = use_sah
+    @debug "Building BVH tree" shapes = length(shapes) max_shapes_per_leaf = max_shapes_per_leaf use_sah = use_sah n_buckets = n_buckets
 
     centroids = [centroid(s) for s in shapes]
 
+    depth = 0
+
     UpdateBoundaries!(root, shapes)
     if use_sah
-        @debug "Subdividing BVH using SAH"
-        SubdivideSAH!(root, shapes, centroids; max_shapes_per_leaf=max_shapes_per_leaf)
+        depth = SubdivideSAH!(root, shapes, centroids, depth; max_shapes_per_leaf=max_shapes_per_leaf, n_buckets=n_buckets)
     else
-        @debug "Subdividing BVH using simple method"
-        Subdivide!(root, shapes, centroids)
+        depth = Subdivide!(root, shapes, centroids, depth)
     end
-
+    @debug "BVH tree built" depth = depth
     return root
 end
 
