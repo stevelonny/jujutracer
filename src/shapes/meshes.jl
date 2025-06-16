4e6f01ef800025434de0dd35e7f33f308553c2b6
Stefano Lonardoni <steve.lonny@outlook.com>
2025-06-16 16:27:46 +0200

    Small adj to docstrings and format

--- src/shapes/meshes.jl
+++ src/shapes/meshes.jl
@@ -16,6 +16,8 @@ mesh.
 - `mesh(file::String, Tr::AbstractTransformation)`: creates a mesh from `file` with a transformation `Tr` 
 - `mesh(file::String, Mat::Material)`: creates a mesh with `Mat` material from `file`
 - `mesh(file::String, Tr::AbstractTransformation, Mat::Material)`: creates a mesh with `Tr` transformation and a `Mat` material.
+For all constructors from obj files, the order of the coordinates in the file is assumed to be "dwh" (depth, width, height) by default.
+If the order is different, you can specify it with the `order` keyword argument.
 """
 struct mesh <: AbstractShape
     file::String
@@ -25,23 +27,23 @@ struct mesh <: AbstractShape
     function mesh(shapes::Vector{Triangle})
         new("file.obj", shapes, Vector{Point}())
     end
-    function mesh(file::String; order = "dwh")
+    function mesh(file::String; order="dwh")
         sh, p = read_obj_file(file; order)
         new(file, sh, p)
     end
 
-    function mesh(file::String, Tr::AbstractTransformation; order = "dwh")
-        sh, p = read_obj_file(file, Tr = Tr; order)
+    function mesh(file::String, Tr::AbstractTransformation; order="dwh")
+        sh, p = read_obj_file(file, Tr=Tr; order)
         new(file, sh, p)
     end
 
-    function mesh(file::String, Mat::Material; order = "dwh")
-        sh, p = read_obj_file(file, Mat = Mat; order)
+    function mesh(file::String, Mat::Material; order="dwh")
+        sh, p = read_obj_file(file, Mat=Mat; order)
         new(file, sh, p)
     end
 
-    function mesh(file::String, Tr::AbstractTransformation, Mat::Material; order = "dwh")
-        sh, p = read_obj_file(file, Tr = Tr, Mat = Mat; order)
+    function mesh(file::String, Tr::AbstractTransformation, Mat::Material; order="dwh")
+        sh, p = read_obj_file(file, Tr=Tr, Mat=Mat; order)
         new(file, sh, p)
     end
 end
@@ -50,22 +52,27 @@ end
     trianglize(P::Vector{Points}, Tr::AbstractTransformation, Mat::Material)
 
 Triangulation method, working only with ordered verteces
+# Arguments
+- `P::Vector{Point}`: the vector of points to be triangulated
+- `Tr::AbstractTransformation`: an hypotetical transformation to be applied to all the shapes and points
+- `Mat::Material`: a material to be applied to all the triangles
+# Returns
+- `tr::Vector{Triangle}`: a vector of triangles created from the points in `P`
 """
 function trianglize(P::Vector{Point}, Tr::AbstractTransformation, Mat::Material)
     tr = Vector{Triangle}()
     p = P[1]
     #sort!(P, by=x -> norm(x - p))
-    for i in 2:(length(P) - 1)
+    for i in 2:(length(P)-1)
         add = Triangle(Tr, p, P[i], P[i+1], Mat)
         push!(tr, add)
     end
     return tr
 end
 """
-
     read_obj_file(io::IOBuffer; Tr::AbstractTransformation = Transformation(), Mat::Material = Material(); order = "dwh")
 
-Method for constructing vectors and points from a buffer
+Method for constructing vectors and points from a buffer.
 # Fields
 - `io::IOBuffer`: the buffer
 - `Tr::AbstractTransformation`: an hypotetical transformation to be applied to all the shapes and points
@@ -76,7 +83,7 @@ Method for constructing vectors and points from a buffer
 - `shape::Vector{Triangle}`
 - `points:Vector{Points}`
 """
-function read_obj_file(io::IOBuffer; Tr::AbstractTransformation = Transformation(), Mat::Material = Material())
+function read_obj_file(io::IOBuffer; Tr::AbstractTransformation=Transformation(), Mat::Material=Material())
     shapes = Vector{Triangle}()
     points = Vector{Point}()
     faces = Vector{Vector{Int}}()
@@ -93,49 +100,49 @@ function read_obj_file(io::IOBuffer; Tr::AbstractTransformation = Transformation
             z = i
         end
     end
-    @debug "Reading OBJ file from IOBuffer with size: $(total) bytes" total=total
-        while !eof(io)
-            line = split(readline(io), ' ')
-            dim = length(line)
+    @debug "Reading OBJ file from IOBuffer with size: $(total) bytes" total = total
+    while !eof(io)
+        line = split(readline(io), ' ')
+        dim = length(line)
 
-            if line[1] == "v"
-                # add a point
-                # account for empty spaces
-                coord = Vector{SubString{String}}()
-                for i in 2:dim
-                    try
-                        parse(Float64, line[i])
-                        push!(coord, line[i])
-                    catch
-                    end
+        if line[1] == "v"
+            # add a point
+            # account for empty spaces
+            coord = Vector{SubString{String}}()
+            for i in 2:dim
+                try
+                    parse(Float64, line[i])
+                    push!(coord, line[i])
+                catch
                 end
-                p = Point(parse(Float64, coord[x]),
-                            parse(Float64, coord[y]),
-                            parse(Float64, coord[z]))
-                push!(points, p)
-            elseif line[1] == "f"
-                # possible formats:
-                # f 1 2 3 ...
-                # f 1/1 2/2 3/3 ...
-                # f 1/1/1 2/2/2 3/3/3 ...
-                # f 1//1 2//2 3//3 ...
-                # face/texture/normal
-                face = Vector{Int}()
-                for i in 2:dim
-                    face_index = split(line[i], '/')
-                    push!(face, parse.(Int, face_index[1]))
-                end
-                push!(faces, face)
-            end #if/else
-        end #while
+            end
+            p = Point(parse(Float64, coord[x]),
+                parse(Float64, coord[y]),
+                parse(Float64, coord[z]))
+            push!(points, p)
+        elseif line[1] == "f"
+            # possible formats:
+            # f 1 2 3 ...
+            # f 1/1 2/2 3/3 ...
+            # f 1/1/1 2/2/2 3/3/3 ...
+            # f 1//1 2//2 3//3 ...
+            # face/texture/normal
+            face = Vector{Int}()
+            for i in 2:dim
+                face_index = split(line[i], '/')
+                push!(face, parse.(Int, face_index[1]))
+            end
+            push!(faces, face)
+        end #if/else
+    end #while
     # Convert faces to triangles
     for face in faces
         if length(face) == 3
             tr = Triangle(Tr,
-                          points[face[1]],
-                          points[face[2]],
-                          points[face[3]],
-                          Mat)
+                points[face[1]],
+                points[face[2]],
+                points[face[3]],
+                Mat)
             push!(shapes, tr)
         elseif length(face) > 3
             P = Vector{Point}()
@@ -152,7 +159,6 @@ function read_obj_file(io::IOBuffer; Tr::AbstractTransformation = Transformation
     return shapes, points
 end
 """
-
     read_obj_file(io::IOBuffer; Tr::AbstractTransformation = Transformation(), Mat::Material = Material(); order = "dwh")
 
 Method for constructing vectors and points from a file
@@ -166,7 +172,7 @@ Method for constructing vectors and points from a file
 - `shape::Vector{Triangle}`
 - `points:Vector{Points}`
 """
-function read_obj_file(filename::String; Tr::AbstractTransformation = Transformation(), Mat::Material = Material())
+function read_obj_file(filename::String; Tr::AbstractTransformation=Transformation(), Mat::Material=Material())
     # Check if the file extension is valid
     if !(endswith(filename, ".obj"))
         throw(InvalidFileFormat("Invalid file extension. Only .obj is supported."))
@@ -177,21 +183,21 @@ function read_obj_file(filename::String; Tr::AbstractTransformation = Transforma
         write(io, file)
     end
     seekstart(io)
-    return read_obj_file(io, Tr = Tr, Mat = Mat; order)
+    return read_obj_file(io, Tr=Tr, Mat=Mat; order)
 end
 
 function ray_intersection(S::mesh, ray::Ray)
     dim = length(S.shapes)
-        closest = nothing
-        for i in 1:dim
-            inter = ray_intersection(S.shapes[i], ray)
-            if isnothing(inter)
-                continue
-            end
-            if (isnothing(closest) || inter.t < closest.t)
-                closest = inter
-            end
+    closest = nothing
+    for i in 1:dim
+        inter = ray_intersection(S.shapes[i], ray)
+        if isnothing(inter)
+            continue
         end
+        if (isnothing(closest) || inter.t < closest.t)
+            closest = inter
+        end
+    end
     return closest
 end
 
