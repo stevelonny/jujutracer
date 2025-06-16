97d200a0f832adfdd47cbc0b51bb61f4c262958b
Stefano Lonardoni <steve.lonny@outlook.com>
2025-06-16 12:01:28 +0200

    clean testBVHTree.jl

--- testBVHTree.jl
+++ testBVHTree.jl
@@ -8,13 +8,6 @@ using Logging
 using TerminalLoggers
 using LoggingExtras
 
-function centroid(t::Triangle)
-    return Point((t.A.x + t.B.x + t.C.x) / 3, (t.A.y + t.B.y + t.C.y) / 3, (t.A.z + t.B.z + t.C.z) / 3)
-end
-function centroid(A::Point, B::Point, C::Point)
-    return Point((A.x + B.x + C.x) / 3, (A.y + B.y + C.y) / 3, (A.z + B.z + C.z) / 3)
-end
-
 # Create a filtered logger
 module_filter(log) = (log._module == jujutracer)
 filtered_logger = EarlyFilteredLogger(module_filter, TerminalLogger(stderr, Logging.Debug))
@@ -35,15 +28,13 @@ pfm_output = filename * ".pfm"
 
 m_tree = mesh("tree.obj", Material(UniformPigment(RGB(0.0, 0.0, 0.0)), DiffusiveBRDF(UniformPigment(RGB(0.5, 0.3, 0.1)))))
 
-tree_shapes = Vector{AbstractShape}()
-for t in m_tree.shapes
-    push!(tree_shapes, t)
+shapes = Vector{AbstractShape}()
+for t in m_mesh.shapes
+    push!(shapes, t)
 end
 
-tree_shapes = deepcopy(tree_shapes)
-
-bvh, primitves = BuildBVH(tree_shapes; use_sah=false)
-bvhshape = BVHShape(bvh, tree_shapes, primitves)
+bvh, bvhdepth = BuildBVH!(shapes; use_sah=true)
+bvhshape = BVHShape(bvh, shapes)
 box = Box(bvhshape.bvhroot.p_min, bvhshape.bvhroot.p_max, Material(UniformPigment(RGB(0.5, 0.3, 0.1)), DiffusiveBRDF(UniformPigment(RGB(0.5, 0.3, 0.1)))))
 
 sun_material = Material(UniformPigment(RGB(1.0, 1.0, 0.8)), DiffusiveBRDF(UniformPigment(RGB(1.0, 1.0, 0.8))))
@@ -54,23 +45,25 @@ A, B = box.P2, box.P1
 spot = SpotLight(A, (B-A), RGB(1.0, 1.0, 0.8), 100.0)
 light = LightSource(A, RGB(0.2, 0.2, 0.12), 100.0)
 
+ground_material = Material(UniformPigment(RGB(0.0, 0.0, 0.0)), DiffusiveBRDF(CheckeredPigment(2, 2, RGB(0.2, 0.2, 0.2), RGB(0.7, 0.7, 0.7))))
 
-shapes = Vector{AbstractShape}()
-push!(shapes, Plane(Material(UniformPigment(RGB(0.0, 0.0, 0.0)), DiffusiveBRDF(UniformPigment(RGB(0.1, 0.2, 0.1))))))
-push!(shapes, sun) # remove it if using point light
-# push!(shapes, box)
-push!(shapes, bvhshape)
+w_shapes = Vector{AbstractShape}()
+push!(w_shapes, Plane(ground_material))
+#push!(w_shapes, sun) # remove it if using point light
+# push!(w_shapes, box)
+push!(w_shapes, bvhshape)
 lights = Vector{AbstractLight}()
 push!(lights, spot)
 push!(lights, light)
 
-world = World(shapes, lights, nothing)
+world = World(w_shapes, lights, nothing)
 
-cam = Perspective(d=1.0, t=Translation(-3.0, 0.0, 7.5), a_ratio=9/16)
+cam = Perspective(d=1.0, t=Translation(-6.0, 0.0, 2.0), a_ratio=16/9)
 hdr = hdrimg(width, height)
 ImgTr = ImageTracer(hdr, cam)
-pcg = PCG()
+#pcg = PCG()
 renderer = Flat(world, RGB(0.1, 0.1, 0.1))
+#renderer = DepthBVHRender(world; bvh_max_depth=bvhdepth)
 #renderer = PathTracer(world, RGB(0.1, 0.1, 0.1), pcg, n_rays, depth, russian)
 #renderer = PointLight(world, RGB(0.1, 0.1, 0.15), RGB(0.1, 0.1, 0.1), 0)
 ImgTr(renderer)
@@ -79,7 +72,3 @@ toned_img = tone_mapping(hdr)
 save_ldrimage(get_matrix(toned_img), png_output)
 write_pfm_image(hdr, pfm_output)
 println("Done")
-
-
-
-
